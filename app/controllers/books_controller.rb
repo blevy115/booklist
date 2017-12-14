d4290e8016758d6fa3bd61f3210d33eeb20f69d4
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-12-14 18:00:15 -0500

    completed exercise

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,13 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    respond_to :html, :text
+
+    respond_to do |format|
+      format.html
+      format.text
+      format.csv {render plain: Book.generate_csv(@books)}
+      format.json {render json: Book.generate_csv(@books)}
+    end
   end
 
 end
